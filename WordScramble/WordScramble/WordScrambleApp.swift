24659f60227d654aac21670700595acaeb5d1b49
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-16 09:56:47 +0200

    init project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by OLEKSANDR ISAIEV on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
